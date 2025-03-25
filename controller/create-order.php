479332570e5f480b6b5f5a920b26d94b375fd9a2
Cyril-Catoen <cyril.catoen@lapiscine.pro>
2025-03-25 16:11:36 +0100

    modification manuelle, car conflit lors du merge develop

--- controller/create-order.php
+++ controller/create-order.php
@@ -11,5 +11,7 @@ if (array_key_exists("quantity", $_POST) && // vérifie que la quantité a été
     $message = "Vous avez commandé " . $quantity . " t-shirt(s) modèle " . $teeshirt . "."; //concatène un message composé de texte prédéfinis et des valeurs récupérés de quantité et du modèle de t-shirt
 }
 
-require_once('../view/create-order-view.php');
+require_once('../view/create-order-view.php'); // récupère le contenu de la view pour afficher le HTML fixe + la partie dynamique / personnalisé par le code PHP. Il faut l'appeler après avoir déclaré la variable $message et le if pour ne pas avoir de code d'erreurs.
+// Si on le require avant $message, le code étant lu de haut en bas, la lecture du code s'interrompt parce que la view appelle la variable $message mais elle n'est pas encore définie.
+
 ?>
