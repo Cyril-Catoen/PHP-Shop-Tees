e8ffb0e1df2a6507bfe576824ff7714b44d50236
Cyril-Catoen <cyril.catoen@lapiscine.pro>
2025-03-25 16:09:51 +0100

    Ajout de commentaires

--- controller/create-order.php
+++ controller/create-order.php
@@ -2,15 +2,16 @@
 
 $message = "";
 
-if (array_key_exists("quantity", $_POST) && 
-    array_key_exists("teeshirt", $_POST)) {
+if (array_key_exists("quantity", $_POST) && // vérifie que la quantité a été saisie ET
+    array_key_exists("teeshirt", $_POST)) { // vérifie que le modèle de t-shirt a été sélectionné
 
-    $quantity = $_POST['quantity'] ;
-    $teeshirt = $_POST['teeshirt'] ;
+    $quantity = $_POST['quantity'] ; // récupère la valeur de la quantité dans l'input quantity une fois le formulaire soumis
+    $teeshirt = $_POST['teeshirt'] ; // recupère la valeur du modèle de t-shirt dans le select teeshirt une fois le formulaire soumis
 
-    $message = "Vous avez commandé " . $quantity . " t-shirt(s) modèle " . $teeshirt . ".";
+    $message = "Vous avez commandé " . $quantity . " t-shirt(s) modèle " . $teeshirt . "."; //concatène un message composé de texte prédéfinis et des valeurs récupérés de quantité et du modèle de t-shirt
 }
 
-require_once('../view/create-order-view.php');
+require_once('../view/create-order-view.php'); // récupère le contenu de la view pour afficher le HTML fixe + la partie dynamique / personnalisé par le code PHP. Il faut l'appeler après avoir déclaré la variable $message et le if pour ne pas avoir de code d'erreurs.
+// Si on le require avant $message, le code étant lu de haut en bas, la lecture du code s'interrompt parce que la view appelle la variable $message mais elle n'est pas encore définie.
 
 ?>
