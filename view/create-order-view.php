7fe7847cac5c5e51c27c90c44173c5323174f0b9
Cyril-Catoen <cyril.catoen@lapiscine.pro>
2025-03-25 14:16:36 +0100

    Ajout du formulaire

---
+++ view/create-order-view.php
@@ -0,0 +1,18 @@
+<?php
+
+require_once('partial/header.php');
+
+?>
+<main>
+    <form method="POST" action="">
+    <div class="w80">
+                <input type="text" name="Quantity" placeholder="Saisissez la quantité">
+        </div>
+</form>
+</main>
+
+<?php
+
+require_once('partial/footer.php');
+
+?>
