234d2224df979c16a44d94412da5701f9fa9b1db
Cyril-Catoen <cyril.catoen@lapiscine.pro>
2025-04-04 16:34:26 +0200

    ajout des messages d'erreur pour cancel and buy suivant status

--- controller/pay-order.controller.php
+++ controller/pay-order.controller.php
@@ -6,13 +6,20 @@ require_once('../model/order-repository.php');
 
 session_start(); // démarre la session, permet de créer un identifiant unique sauvegardé dans les cookies du navigateur de l'utilisateur
 
-
+$message = "";
 $orderForUser = findOrderByUser(); // on récupère la commande du client 
 
 if ($_SERVER['REQUEST_METHOD'] === 'POST') { // Si le serveur récupère des données POST (on appuie sur le bouton pay), on exécute le code suivant :
-    $orderForUser['status'] = "PAID"; // on change le statut de la commande de l'utiisateur de CART à PAID
-    saveOrder($orderForUser); // on sauvegarde la commande après le changement de statut pour prendre en compte le fait que le client à payer
+    if ($orderForUser['status'] == "CART") {
+        $orderForUser['status'] = "PAID"; // on change le statut de la commande de l'utiisateur de CART à PAID
+        saveOrder($orderForUser); // on sauvegarde la commande après le changement de statut pour prendre en compte le fait que le client à payer
+    }
+
+    else {
+        $message = "Votre commande a été annulée. Veuillez faire une autre commande.";
+    }
 }
+
 
 
 require_once('../view/pay-order.view.php');
--- view/pay-order.view.php
+++ view/pay-order.view.php
@@ -10,10 +10,13 @@ require_once('partial/header.php');
             <p>Vous avez commandé : <?php echo $orderForUser['quantity'] ?> : <?php echo $orderForUser['teeshirt'] ?></p>
             <p>Créée le : <?php echo $orderForUser['created_at']?></p>
             <p>Status : <?php echo $orderForUser['status'] ?></p>
+            <?php if ($message){
+                        echo "<br><p class='red'>" . $message . "</p>";
+                    } ?>
         <form method="POST" action=""><button class="submit2" type="submit">Payer</button></form>
-        <?php } else { ?>
+            <?php } else { ?>
             <p> Vous n'avez pas réalisé de commande.</p>
-      <?php  } ?>
+            <?php  } ?>
 </main>
 
 <?php
