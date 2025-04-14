a8d1cb2ddec533bbd5f074bf7727dc6233c0f26f
Cyril-Catoen <cyril.catoen@lapiscine.pro>
2025-04-14 10:18:19 +0200

    modification status

--- controller/ship-order.controller.php
+++ controller/ship-order.controller.php
@@ -10,14 +10,11 @@ $orderForUser = findOrderByUser(); // on récupère la commande du client
 
 if ($_SERVER['REQUEST_METHOD'] === 'POST') { // Si le serveur récupère des données POST (on appuie sur le bouton expédier), on exécute le code suivant :
     
-    if ($orderForUser['status'] == "PAID") {
     $orderForUser['status'] = "SENT"; // on change le statut de la commande de l'utiisateur de PAID à SENT
     saveOrder($orderForUser); // on sauvegarde la commande après le changement de statut pour prendre en compte le fait que le client à payer
-    } 
-    
-    else {
+  
     $message = "Votre commande a été expédiée.";
-    }
+    
 }
 
 
