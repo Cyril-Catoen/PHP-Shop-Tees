f58030d2c1b8fcb72100b0b0da36b6fe64832636
Cyril-Catoen <cyril.catoen@lapiscine.pro>
2025-04-14 12:29:10 +0200

    création d'une classe Order et d'un objet

--- model/order.model.php
+++ model/order.model.php
@@ -5,4 +5,14 @@ class Order {
     public int $quantity;
     public DateTime $createdAt;
     public string $status; 
+
+// Je créé un nouvel objet avec un ensemble de propriétés à définir
+$order = new Order();
+
+// Je définis la valeur des propriétés de ce nouvel objet
+$order->product = "Teeshirt Mathieu Rozand";
+$order->quantity = 3;
+$order->createdAt = new DateTime();
+$order->status = "Cart";
 }?>
+
